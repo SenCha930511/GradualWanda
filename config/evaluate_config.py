0c8e5c8cc0a2f5bb04edbe96d25f09658ee3e5ae
SenCha930511 <sencha930511@gmail.com>
2025-03-24 13:27:07 +0800

    Refactor project structure

--- config/evaluate_config.py
+++ config/evaluate_config.py
@@ -2,8 +2,8 @@ from dataclasses import dataclass
 from typing import List
 @dataclass
 class EvaluateConfig:
-    ntrain : int
-    data_dir : str
+    ntrain : int = 5
+    data_dir : str = "data"
     save_dir : str
     engine : List[str]
     model_base : str
