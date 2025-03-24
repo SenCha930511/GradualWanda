3c89d6c50afcbed48c4968ca80762753120e65d9
SenCha930511 <sencha930511@gmail.com>
2025-03-24 13:49:41 +0800

    Add EvalConfig

--- config/evaluate_config.py
+++ config/evaluate_config.py
@@ -4,6 +4,12 @@ from typing import List
 class EvaluateConfig:
     ntrain : int = 5
     data_dir : str = "data"
-    save_dir : str
+    save_dir : str = "result"
     engine : List[str] = ["llama2"]
-    model_base : str
+    model_base : str = "/media/GradualWanda/merged_model"
+
+    @staticmethod
+    def default():
+        return EvaluateConfig(
+            save_dir = "result" + model_base
+        )
