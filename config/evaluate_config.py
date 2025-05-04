eb87f78aa136973411a02eefa373d719949e2fb8
Anson6118 <et6118.ans@gmail.com>
2025-05-04 11:36:57 +0800

    Update data structure

--- config/evaluate_config.py
+++ config/evaluate_config.py
@@ -8,9 +8,12 @@ class EvaluateConfig:
     data_dir : str = "data"
     save_dir : str = "output"   #直接用output，雖然本來是result
     engine : List[str] = ["llama2"]
+<<<<<<< HEAD
 =======
     ntrain: int = 5          # 用於提示詞的訓練範例數量
     data_dir: str = "data"    # 包含開發/測試 CSV 檔案的目錄
     save_dir: str = "output"  # 儲存評估結果的目錄（你可以自行調整）
     engine: List[str] = ("merged",)  # 引擎標籤
 >>>>>>> Stashed changes
+=======
+>>>>>>> 0d5d81d (Update data structure)
